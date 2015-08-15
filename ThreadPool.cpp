7fc15228460b9cbb4b1e9f7e409a7be1ba3d9451
work_ubuntu_VM_kouen <jobkouen@outlook.com>
2015-08-15 08:11:21 -0700

    多线程新一轮草图

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,1 +1,23 @@
 #include "ThreadPool.h"
+#include "Event.h"
+
+void ThreadPool::Run()
+{
+	Event event;
+	while(looping_){
+		while(queues_[IN].pop(event)){
+			//生成EventFunc对象	
+			//执行EventFunc对象
+			//将三个队列作为参数传递到里面，重新修改FuncPtr的参数为void (boost::shared_ptr<Event>&, boost::lockfree::queue<boost::shared_ptr<Event> > (&)[3])
+			/*
+			 * 在EventLoop中修改：
+			 *		1.首先将所有事件都插入到queues[IN]，然后从queues[OUT]读取需要发送的事件，从queues[ERROR]中读取没有对应执行函数的事件
+			 *		2.所有到queues[OUT]都是IO事件，其中需要包含几个信息：
+			 *			1.发送内容
+			 *			2.发送的句柄
+			 *		3.将EventFunc移植出去
+			 *		4.将所有FuncPtr参数进行修改，并且修改对应的处理函数
+			 */
+		}
+	}
+}
--- ThreadPool.h
+++ ThreadPool.h
@@ -1,6 +1,45 @@
 #pragma once
 
+#include <boost/lockfree/queue.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+
+#define IN 1
+#define OUT 2
+#define ERROR 3
+
+class Event;
+
 class ThreadPool
 {
+private:
+	boost::lockfree::queue<boost::shared_ptr<Event> > (&queues_)[3];
+	int threadCount_;
+	bool looping_;
+
+	vector<boost::thread*> vecpThrd_;
+public:
+	ThreadPool(int threadCount, boost::lockfree::queue<boost::shared_ptr<Event> > (&queues)[3]):
+		queues_(queues),
+		threadCount_(threadCount),
+		looping_(true){
+		boost::function0<void> f = boost::bind(&ThreadPool::Run, this);
+		for (int i = 0; i < threadCount_; i++){
+			boost::thread* thrd = new boost::thread(f);
+			vecpThrd_.push_back(thrd);
+		}
+	}
+	~ThreadPool(){
+		for (int i = 0; i < threadCount_; i++){
+			vecpThrd_[i]->join();
+		}
+	}
+
+	void shutdown(){
+		looping_ = false;
+	}
+
+	void Run();
 
 };
